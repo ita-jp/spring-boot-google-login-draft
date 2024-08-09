02b52f5c4234e6381b5f990f0eb6439b31bf0889
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-08-09 09:47:28 +0900

    Spring Boot アプリケーションの設定

--- src/main/java/com/example/oidc/config/SecurityConfig.java
+++ src/main/java/com/example/oidc/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.oidc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,7 +17,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
