f4d6b51310c60eaeab9f0cccb2b79d5aa9fa11ba
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-08-09 22:54:17 +0900

    UserService#register から UserRepository#insert を呼び出す

--- src/main/java/com/example/oidc/service/UserService.java
+++ src/main/java/com/example/oidc/service/UserService.java
@@ -1,15 +1,20 @@
 package com.example.oidc.service;
 
+import com.example.oidc.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
 
     @Transactional
     public void register(String username) {
         var newUser = new UserEntity(null, username);
-        // TODO
+        userRepository.insert(newUser);
     }
 
 }
