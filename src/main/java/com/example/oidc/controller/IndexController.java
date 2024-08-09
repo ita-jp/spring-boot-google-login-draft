7cb575005c971824920cb0e527c09f007fc8f239
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-08-09 16:29:48 +0900

    tmp

--- src/main/java/com/example/oidc/controller/IndexController.java
+++ src/main/java/com/example/oidc/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.example.oidc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -35,6 +36,11 @@ public class IndexController {
     @GetMapping("register-profile")
     public String registerProfilePage() {
         return "register-profile";
+    }
+
+    @PostMapping("/register-profile")
+    public String registerProfile() {
+        return "redirect:/";
     }
 
 }
