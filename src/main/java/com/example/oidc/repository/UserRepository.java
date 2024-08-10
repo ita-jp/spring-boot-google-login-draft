30d4db0d85c916db5aa77a1d33adea59e45c7bc2
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-08-10 10:35:11 +0900

    OIDC の subject を指定してデータベースからユーザー情報を取得するメソッドを実装する

--- src/main/java/com/example/oidc/repository/UserRepository.java
+++ src/main/java/com/example/oidc/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.oidc.service.UserEntity;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
 
 @Mapper
 public interface UserRepository {
@@ -13,4 +16,15 @@ public interface UserRepository {
             """)
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     void insert(UserEntity entity);
+
+    @Select("""
+            SELECT
+                u.id
+              , u.username
+            FROM users u
+            JOIN user_social_logins usl ON u.id = usl.user_id
+            WHERE
+                usl.subject = #{subject}
+            """)
+    Optional<UserEntity> selectBySubject(String provider, String subject);
 }
